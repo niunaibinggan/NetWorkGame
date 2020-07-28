7a70a774b06d1c5e46afde3faddf8146160afe0c
王悦 <1yue.wang@zhaopin.com.cn>
2020-07-28 21:14:48 +0800

    缩略图

--- components/game/save.js
+++ components/game/save.js
@@ -1,6 +1,8 @@
 import AssetsFectory from '~/components/game/asset'
 import StageFectory from '~/components/game/stage'
 import ExportScence from '~/components/game/exportScence'
+import SubmitButton from '~/components/game/submitButton'
+import Panel from '~/components/game/panel'
 
 export default async function init (questions) {
   // const  // 接入hilo动画引擎
@@ -25,7 +27,36 @@ export default async function init (questions) {
     title: questions.title,
   })
 
+  // 准备按钮 canvas 1920 1080 , subButton 329 96
+  const subBtn = new SubmitButton({
+    x: (1920 - 329) / 2,
+    y: (1080 - 96) / 2 + 470,
+    images: assets.submitButton,
+    rect: [0, 0, 329, 96],
+    visible: true,
+  })
+
+  // 题目
+  const { questionLeft, questionRight, errorLine, rightLine, questionsImage, errorIcon, tipsLine } = assets
+  // 插入题目 两个板块之间的距离 300 每个背景板的长度 499 106
+  const panel = new Panel({
+    // x: (1920 - 499 * 2 - 300) / 2,
+    // y: 320 - (this.questions.left.length * 10),
+    x: 0,
+    y: 0,
+    images: { questionLeft, questionRight, errorLine, rightLine, questionsImage, errorIcon, tipsLine },
+    questions: questions,
+    answerQuestionsIds: [],
+    answerRealIds: [],
+    resultIds: [],
+    type: 'panel',
+    stage: stage,
+    submitBtn: subBtn
+  })
+
   stage.addChild(exportScence)
+  stage.addChild(subBtn)
+  stage.addChild(panel)
 
   return new Promise((re) => {
     ticker.nextTick(() => {
@@ -33,3 +64,4 @@ export default async function init (questions) {
     })
   })
 }
+
